2d36519ec4148dcf4cdfcb90724df11295adc24a
Ezermod <ppolaralves@gmail.com>
2026-09-24 12:10:07 -0300

    Add system.prop support to device.mk

--- device.mk
+++ device.mk
@@ -6,6 +6,9 @@
 # A/B
 TARGET_IS_VAB := true
 
+# add system.prop suport
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
